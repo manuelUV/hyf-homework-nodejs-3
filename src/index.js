98b3a15b99d8c31c8eb87ed0255439d56e017879
Juan Manuel Garcia <jmgarcia@unitedvirtualities.com>
2020-02-05 13:02:41 -0600

    step 2

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ const app = express();
 app.get('/', function(req,res){
     res.send('Hello World!');
 });
-
+app.get('/users', function(req,res){
+    res.json({});
+});
 
 app.listen(3000,function(){console.log('server is listening')})
